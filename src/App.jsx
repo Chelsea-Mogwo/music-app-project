c38286549b1613ef969ed41dcc831183b97cd733
Chelsea Mogwo <chelseamogwo@gmail.com>
2023-08-29 12:02:18 +0100

    add song list component

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,46 @@
 import React, { useState } from "react"
 
-export default function artistName() {
+function SongList() {
 
-    const [name, setName] = useState("")
+  return (
+    <>
+      <ul>
+        <li>7, release date: 2016 <img src="#"></img></li>
+        <li>Anything, release date: 2016 <img src="#"></img></li>
+        <li>Cocoon, release date: 2014 <img src="#"></img></li>
+        <li>Fallout, release date: 2014 <img src="#"></img></li>
+        <li>Homesick, release date: 2014 <img src="#"></img></li>
+        <li>Hourglass, release date: 2014 <img src="#"></img></li>
+        <li>Kathleen, release date: 2014 <img src="#"></img></li>
+        <li>Pacifier, release date: 2014 <img src="#"></img></li>
+        <li>Twice, release date: 2016 <img src="#"></img></li>
+        <li>Soundcheck, release date: 2016 <img src="#"></img></li>
+      </ul>
+    </>
+  )
+}
 
-    return (
-        <>
-          <h1>Arctic Monkeys</h1>
-          <h2>Alt</h2>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
-            Sunt laborum ducimus architecto cupiditate voluptates porro 
-            error, magnam dolorem nam iusto.</p>
-        </>
-    )
+function ArtistInfo() {
+  return (
+    <>
+      <h1>Catfish and the Bottlemen</h1>
+      <h2>Alt</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+        Sunt laborum ducimus architecto cupiditate voluptates porro 
+        error, magnam dolorem nam iusto.</p>
+      <SongList />
+    </>
+  )
+}
+
+export default function Artist() {
+
+  const [name, setName] = useState("")
+
+  return (
+    <>
+    <ArtistInfo />
+    </>
+  )
+
 }
