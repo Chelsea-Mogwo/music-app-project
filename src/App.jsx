a53367e6af8a70c2c56b51cced975d77a6c6f379
Chelsea Mogwo <chelseamogwo@gmail.com>
2023-08-29 12:37:26 +0100

    add a like button

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,50 @@
 import React, { useState } from "react"
+import './App.css'
+
+function LikeButton() {
+  const [like, setLike] = useState("Like")
+
+  const handleLikedButton = () => {
+    setLike(previous => "Unlike")
+  }
+
+  const handleUnlikedButton = () => {
+    setLike(previous => "Like")
+  }
+
+  const handleButton = () => {
+    if (like === "Like") {
+      handleLikedButton()
+    } else {
+      handleUnlikedButton()
+    }
+  }
+
+  return (
+    <>
+      <button
+        onClick={handleButton}
+      >{like}</button>
+    </>
+  )
+}
+  
 
 function SongList() {
 
   return (
     <>
       <ul>
-        <li>7, release date: 2016 <img src="#"></img></li>
-        <li>Anything, release date: 2016 <img src="#"></img></li>
-        <li>Cocoon, release date: 2014 <img src="#"></img></li>
-        <li>Fallout, release date: 2014 <img src="#"></img></li>
-        <li>Homesick, release date: 2014 <img src="#"></img></li>
-        <li>Hourglass, release date: 2014 <img src="#"></img></li>
-        <li>Kathleen, release date: 2014 <img src="#"></img></li>
-        <li>Pacifier, release date: 2014 <img src="#"></img></li>
-        <li>Twice, release date: 2016 <img src="#"></img></li>
-        <li>Soundcheck, release date: 2016 <img src="#"></img></li>
+        <li>7, release date: 2016 <img src="#"></img> <LikeButton /></li>
+        <li>Anything, release date: 2016 <img src="#"></img> <LikeButton /></li>
+        <li>Cocoon, release date: 2014 <img src="#"></img> <LikeButton /></li>
+        <li>Fallout, release date: 2014 <img src="#"></img> <LikeButton /></li>
+        <li>Homesick, release date: 2014 <img src="#"></img> <LikeButton /></li>
+        <li>Hourglass, release date: 2014 <img src="#"></img> <LikeButton /></li>
+        <li>Kathleen, release date: 2014 <img src="#"></img> <LikeButton /></li>
+        <li>Pacifier, release date: 2014 <img src="#"></img> <LikeButton /></li>
+        <li>Twice, release date: 2016 <img src="#"></img> <LikeButton /></li>
+        <li>Soundcheck, release date: 2016 <img src="#"></img> <LikeButton /></li>
       </ul>
     </>
   )
